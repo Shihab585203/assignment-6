360900fc9f329ccb81758f923286a72cbf32038b
Shihab585203 <freelancersehab1@gmail.com>
2022-09-03 20:59:16 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Indicate Page Location
+document.getElementById('news-menubar').addEventListener('click', function(){
+    window.location.href = 'index.html';
+})
+document.getElementById('logo').addEventListener('click', function(){
+    window.location.href = 'index.html';
+})
+
+document.getElementById('blog-menubar').addEventListener('click', function(){
+    window.location.href = 'blog.html';
+})
+
+//Load Category API
+
+const loadCategory = () =>{
+    
+}
