154e4f8d4f99881ddaab3804f3ce72ad4e358510
Shihab585203 <freelancersehab1@gmail.com>
2022-09-04 10:42:34 +0600

    push for third times

--- js/app.js
+++ js/app.js
@@ -1,63 +1,136 @@
 // Indicate Page Location
-document.getElementById('news-menubar').addEventListener('click', function(){
-    window.location.href = 'index.html';
-})
-document.getElementById('logo').addEventListener('click', function(){
-    window.location.href = 'index.html';
-})
+// document.getElementById('news-menubar').addEventListener('click', function(){
+//     window.location.href = 'index.html';
+// })
+// document.getElementById('logo').addEventListener('click', function(){
+//     window.location.href = 'index.html';
+// })
 
-document.getElementById('blog-menubar').addEventListener('click', function(){
-    window.location.href = 'blog.html';
-})
+// document.getElementById('blog-menubar').addEventListener('click', function(){
+//     window.location.href = 'blog.html';
+// })
 
-//Load Category API
-
-const loadCategory = async () =>{
-    
-    const url = `https://openapi.programming-hero.com/api/news/categories`;
-    const res = await fetch(url);
-    const data = await res.json();
+const loadAllData = async() =>{
+    const url = ` https://openapi.programming-hero.com/api/news/categories`
     try{
-        if(data == "") throw "empty";
-    } catch (error){
-        console.log("error");
+      const res = await fetch(url)
+      const data = await res.json()
+      displayAllData(data.data.news_category)
     }
-    displayCategory(data.data.news_category);
+    catch (error){
+      alert('Something Went Wrong')
+    }
+   
 }
+loadAllData()
 
-const displayCategory = categories => {
-    console.log(categories);
-    const categoryContainer = document.getElementById('category-container');
-    categories.forEach(category => {
-        const categoryMenu = document.createElement('h5');
-        categoryMenu.innerHTML = `
-            <a onclick="loadNews(${category.category_id})" class="category-name text-decoration-none text-muted" href="#">${category.category_name}</a>
-        `;
-        categoryContainer.appendChild(categoryMenu);
-    });
-}
-
-// Load News API
-
-
-const loadNews = async (news_id) =>{
+const displayAllData = allNews =>{
+  
+   const place = document.getElementById('navbar-news')
+    allNews.forEach(news =>{
+     
+       const div = document.createElement('div')
+       div.innerHTML = `
+       <a id="show-all-news-length" onclick="showDetails('${news.category_id}')" class="nav-link" href="#">${news.category_name}</a>
+       `   
+       place.appendChild(div)
+    })
     
-    const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
-    const res = await fetch(url);
-    const data = await res.json();
+}
+ 
+// show details 
+const showDetails = async(id) =>{
+  loadSpinner(true)
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
     try{
-        if(data == "") throw "empty";
-    } catch (error){
-        console.log("error");
+      const res = await fetch(url)
+      const data = await res.json()
+      displayDetails(data.data)
     }
-    displayNews(data.data[0]);
+    catch (error){
+      alert('Something wrong')
+    }
+   }
+// display show detail
+const displayDetails = categoryId =>{
+  categoryId.sort((a,b) =>{
+    return b.total_view - a.total_view
+  })
+  const showNews = document.getElementById('show-all-news').innerText = categoryId.length? categoryId.length : 'No news'
+    const placeCard = document.getElementById('card-container')
+    placeCard.innerHTML = '';
+    categoryId.forEach(id =>{
+        // console.log(id)
+
+        const cardDiv = document.createElement('div')
+        cardDiv.classList.add('col')
+        cardDiv.innerHTML = `
+        <div class="card mb-3 onclick="detailModal('${id._id}')" data-bs-toggle="modal"data-bs-target="#exampleModal"" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${id.thumbnail_url}" class="img-fluid rounded-start" alt="">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${id.title}</h5>
+        <p class="card-text cards">${id.details}</p>
+        <div class="d-flex justify-content-around align-items-center">
+       <div> <img class="img-fluid img" src="${id.author.img}" alt="">  ${id.author.name ? id.author.name : "No found name"}</div>
+       <div class=""> 
+      <p>  <p/>
+      <p> <i class="fa-solid fa-eye"></i> ${id.total_view ? id.total_view : 'No data available'}</p>
+       </div>
+       <a onclick="detailModal('${id._id}')" data-bs-toggle="modal"data-bs-target="#exampleModal">  <i class="fa-solid fa-arrow-right"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+        `
+        placeCard.appendChild(cardDiv)
+    })
+    loadSpinner(false)
 }
 
-
-const displayNews = news => {
-    console.log(news); 
+const detailModal = async (dataId) =>{
+    const url = `https://openapi.programming-hero.com/api/news/${dataId}`
+    try{
+      const res = await fetch(url)
+      const data = await res.json()
+      displayModal(data.data)
+    }
+    catch (error){
+      alert('Something went wrong')
+    }
+    
+    
+}
+const displayModal = id =>{
+   
+    id.forEach(newsId =>{
+      console.log(newsId)
+      const modalTitle = document.getElementById('exampleModalLabel')
+      modalTitle.innerText = newsId.title;
+      const modalBody = document.getElementById('modal-body')
+      modalBody.innerHTML = ` 
+      <img class="img" src="${newsId.author.img}" alt="">
+      <P>${newsId.author.name ? newsId.author.name : 'No found name'} </br> ${newsId.author.published_date}
+      <hr>
+      <p><i class="fa-solid fa-eye"></i> ${newsId.total_view ? newsId.total_view : 'No data available'} </p>
+      <p>${newsId.title}</p>
+      <img class="img-fluid" src="${newsId.thumbnail_url}" alt="">
+      `
+    })  
 }
 
-loadNews();
+//loading spinner
+const loadSpinner = isLoading =>{
+  const loader = document.getElementById('loader')
+  if(isLoading){
+    loader.classList.remove('d-none')
+  }
+  else{
+    loader.classList.add('d-none')
+  }
+}
 
-loadCategory();
